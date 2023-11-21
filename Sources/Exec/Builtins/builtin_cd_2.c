c4c72211fc33aa0d424081243820800cad0d501c
Marine LANCAR <malancar@bess-f2r2s5.clusters.42paris.fr>
2023-11-21 19:05:35 +0100

    add file

---
+++ Sources/Exec/Builtins/builtin_cd_2.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin_cd_2.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: auferran <auferran@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/21 17:03:30 by auferran          #+#    #+#             */
+/*   Updated: 2023/11/21 17:03:31 by auferran         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "exec.h"
+#include "minishell.h"
+
+int	its_oldpwd(char *str)
+{
+	int		i;
+	char	*old_pwd;
+
+	i = 0;
+	old_pwd = "OLDPWD";
+	while (str[i] && old_pwd[i] && str[i] == old_pwd[i])
+		i++;
+	if (str[i] && str[i] == '=' && !old_pwd[i])
+		return (1);
+	return (0);
+}
+
+int	its_pwd(char *str)
+{
+	int		i;
+	char	*pwd;
+
+	i = 0;
+	pwd = "PWD";
+	while (str[i] && pwd[i] && str[i] == pwd[i])
+		i++;
+	if (str[i] && str[i] == '=' && !pwd[i])
+		return (1);
+	return (0);
+}
